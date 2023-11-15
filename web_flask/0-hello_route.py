249539a25006b72fd2aebcbabcdfa4a34434e9a3
khaoula-berrah <berah.khaoula@gmail.com>
2023-11-15 22:47:18 +0000

    tt0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/airbnb-onepage/', strict_slashes = False)
 def hello_hbnb():
     """print web"""
-    return render_template("10-hbnb_filters.html")
+    return "HELLO HBNB"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=None)
+"""app.run(debug=True)"""
+    app.run(host='0.0.0.0', port=5000)
